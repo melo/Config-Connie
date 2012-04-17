6143a01d04f52c055c85774e50d167d9f4154e9e
Pedro Melo <melo@simplicidade.org>
2012-04-17 17:43:18 +0100

    Add a bit of documentation
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Config/Connie.pm
+++ lib/Config/Connie.pm
@@ -1,11 +1,52 @@
 package Config::Connie;
 
 # ABSTRACT: a very cool module
+# VERSION
+# AUTHORITY
 
 use strict;
 use warnings;
 
-# VERSION
-# AUTHORITY
+
+
+=encoding utf8
+
+=head1 DESCRIPTION
+
+Configuration is organized on three level system:
+
+=over 4
+
+=item application
+
+The application identifier.
+
+=item environment
+
+The environment to use, like production, development, or staging.
+
+=item configuration key
+
+A arbitrary string.
+
+=back
+
+Each application can have multiple environments. Each application
+environment can have multiple configuration key, and each one has a
+configuration hash.
+
+
+=head2 Components
+
+The system has two components: client and manager.
+
+Apps use a client component to access configuration keys and to listen
+for changes Connie provides utilities to manage these objects as
+singletons
+
+Manager allows you to list available configuration keys, get and update them.
+
+
+=cut
 
 1;
