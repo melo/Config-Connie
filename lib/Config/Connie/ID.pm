fd25267b317dd4070309d67737024e811d3b1e0d
Pedro Melo <melo@simplicidade.org>
2012-08-13 12:36:43 +0100

    Simplify Config::Connie::ID
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Config/Connie/ID.pm
+++ lib/Config/Connie/ID.pm
@@ -5,7 +5,6 @@ use namespace::autoclean;
 
 requires 'default_config_id';
 
-has 'id' => (is => 'lazy', builder => 1);
-sub _build_id { shift->default_config_id }
+has 'id' => (is => 'lazy', builder => 'default_config_id');
 
 1;
